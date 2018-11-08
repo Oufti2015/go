07914942a3189a83135fd5bbee8e2619b1d4f963
Stéphane Stiennon <stephane.stiennon@gmail.com>
2018-11-08 14:39:37 +0100

    Lesson 21

--- publish.sh
+++ publish.sh
@@ -2,12 +2,15 @@
 
 echo Publishing $1 as Lesson $2
 
+echo -----------------------
 echo Add $1
 git add $1
 
+echo -----------------------
 echo Commit "Lesson $2"
 git commit -m "Lesson $2"
 
+echo -----------------------
 echo Push origin
 git push origin
 
